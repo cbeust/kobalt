4f87c14da37ddb1004d1c2c0c32a922923703a57
Cedric Beust <cedric@beust.com>
2015-10-14 00:17:42 -0700

    Syntax.

--- src/main/kotlin/com/beust/kobalt/misc/Benchmarks.kt
+++ src/main/kotlin/com/beust/kobalt/misc/Benchmarks.kt
@@ -3,6 +3,6 @@ package com.beust.kobalt.misc
 public fun benchmark(message: String, run: () -> Unit) {
     val start = System.currentTimeMillis()
     run()
-    println("############# Time to ${message}: ${System.currentTimeMillis() - start} ms")
+    println("############# Time to $message: ${System.currentTimeMillis() - start} ms")
 }
 
