5b3ef5e91916e8e88c3ce02ad515d0e76cc9b294
Cedric Beust <cedric@beust.com>
2015-10-21 23:52:37 -0700

    Javadoc.

--- src/main/kotlin/com/beust/kobalt/internal/remote/PingCommand.kt
+++ src/main/kotlin/com/beust/kobalt/internal/remote/PingCommand.kt
@@ -2,6 +2,14 @@ package com.beust.kobalt.internal.remote
 
 import com.google.gson.JsonObject
 
+/**
+ * A simple command that returns its own content.
+ * Payload:
+ * {
+ *   "name" : "ping"
+ * }
+ *
+ */
 class PingCommand() : ICommand {
     override val name = "ping"
     override fun run(sender: ICommandSender, received: JsonObject) =
