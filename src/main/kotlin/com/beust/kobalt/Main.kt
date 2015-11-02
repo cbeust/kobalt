5027d48fce2830feff1d6df852bbd3586d4c6797
Cedric Beust <cedric@beust.com>
2015-11-01 20:52:09 -0800

    Better error.

--- src/main/kotlin/com/beust/kobalt/Main.kt
+++ src/main/kotlin/com/beust/kobalt/Main.kt
@@ -120,7 +120,7 @@ private class Main @Inject constructor(
                 try {
                     allProjects = buildFileCompilerFactory.create(listOf(buildFile)).compileBuildFiles(args)
                 } catch(ex: Throwable) {
-                    error("Couldn't build", ex)
+                    error("Couldn't build", ex.cause ?: ex)
                     log(2, "Couldn't parse preBuildScript.jar: ${ex.message}")
 //                    if (! File(".kobalt").deleteRecursively()) {
 //                        warn("Couldn't delete .kobalt, please delete it manually")
