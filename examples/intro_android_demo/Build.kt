79c8669b091ed1a8bc49dee714ccdde5752a89db
Cedric Beust <cedric@beust.com>
2015-11-04 21:30:05 -0800

    Examples.

---
+++ examples/intro_android_demo/Build.kt
@@ -0,0 +1,27 @@
+import com.beust.kobalt.*
+import com.beust.kobalt.plugin.android.*
+import com.beust.kobalt.plugin.java.*
+
+val r = repos(file("d:\\android\\adt-bundle-windows-x86_64-20140321\\sdk\\extras\\android\\m2repository"))
+
+val p = javaProject {
+    name = "intro_android_demo"
+    group = "com.example"
+    artifactId = name
+    version = "0.1"
+    directory = homeDir("android/intro_android_demo")
+
+    sourceDirectories {
+        listOf(path("app/src/main/java"))
+    }
+
+    dependencies {
+        compile(file("app/libs/android-async-http-1.4.3.jar"),
+            "com.android.support:support-v4:aar:23.0.1")
+    }
+
+    android {
+        applicationId = name
+        buildToolsVersion = "21.1.3"
+    }
+}
