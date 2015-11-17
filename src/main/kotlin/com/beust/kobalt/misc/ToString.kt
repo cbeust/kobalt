57526999564b338de17539dbf70652a7082a63aa
Cedric Beust <cedric@beust.com>
2015-11-17 06:54:39 -0800

    Update syntax.

--- src/main/kotlin/com/beust/kobalt/misc/ToString.kt
+++ src/main/kotlin/com/beust/kobalt/misc/ToString.kt
@@ -3,9 +3,11 @@ package com.beust.kobalt.misc
 fun <T> toString(name: String, vararg o: T) : String {
     val sb = StringBuilder()
 
-    for (i in 0..o.size - 1 step 2) {
+    var i = 0
+    while (i < o.size) {
         if (i > 0) sb.append(", ")
         sb.append(o[i].toString() + ":" + o[i + 1])
+        i += 2
     }
     return "{$name $sb}"
 }
