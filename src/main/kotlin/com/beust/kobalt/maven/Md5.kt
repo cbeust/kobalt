7aaffc03e11b543866550989a1eab037d558634a
Cedric Beust <cedric@beust.com>
2015-11-17 21:30:40 -0800

    Fix MD5.

--- src/main/kotlin/com/beust/kobalt/maven/Md5.kt
+++ src/main/kotlin/com/beust/kobalt/maven/Md5.kt
@@ -1,29 +1,16 @@
 package com.beust.kobalt.maven
 
+import org.jetbrains.kotlin.rmi.toHexString
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.security.MessageDigest
 
 public class Md5 {
     companion object {
-        fun toMd5(file: File) =
-                MessageDigest.getInstance("MD5").let { md5 ->
-                    file.forEachBlock { bytes, size ->
-                        md5.update(bytes, 0, size)
-                    }
-                    md5.digest().toHex()
+        fun toMd5(file: File) = MessageDigest.getInstance("MD5").let { md5 ->
+                file.forEachBlock { bytes, size ->
+                    md5.update(bytes, 0, size)
                 }
-
-        fun toMd5(bytes: ByteArray): String =
-                MessageDigest.getInstance("MD5").digest(bytes).toHex()
-    }
-}
-
-private fun ByteArray.toHex() = buildString {
-    forEach {
-        val byte = it.toInt() and 0xff
-        if (byte < 16) append("0")
-        append(Integer.toHexString(byte))
+                md5.digest().toHexString()
+            }
     }
 }
