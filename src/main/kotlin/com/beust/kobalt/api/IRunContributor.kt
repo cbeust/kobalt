3e6760f8f527dff1baa8fa9c028036685eb76fca
Cedric Beust <cedric@beust.com>
2015-11-23 22:51:57 -0800

    Better documentation. 

--- src/main/kotlin/com/beust/kobalt/api/IRunContributor.kt
+++ src/main/kotlin/com/beust/kobalt/api/IRunContributor.kt
@@ -3,10 +3,14 @@ package com.beust.kobalt.api
 import com.beust.kobalt.TaskResult
 
 /**
- * Plugins that can implement the "task" run should implement this interface.
+ * Plugins that can run a project (task "run") should implement this interface.
  */
 interface IRunContributor : IContributor {
     companion object {
+        /**
+         * The recommended default affinity if your plug-in can run this project. Use a higher
+         * number if you expect to compete against other runners and you'd like to win over them.
+         */
         const val DEFAULT_POSITIVE_AFFINITY = 100
     }
 
