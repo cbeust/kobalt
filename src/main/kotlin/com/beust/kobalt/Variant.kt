37a0f3101aee13b66579a5c3f25ff02b8f7ac553
Cedric Beust <cedric@beust.com>
2015-11-24 20:59:28 -0800

    Revert "Refactor."
    This reverts commit e5d03956f91d6319f08d7884be2879f2efe875fe.

--- src/main/kotlin/com/beust/kobalt/Variant.kt
+++ src/main/kotlin/com/beust/kobalt/Variant.kt
@@ -86,7 +86,8 @@ class Variant(val initialProductFlavor: ProductFlavorConfig? = null,
      * If either the Project or the current variant has a build config defined, generate BuildConfig.java
      */
     fun maybeGenerateBuildConfig(project: Project, context: KobaltContext) {
-        fun generated(project: Project) = KFiles.joinDir(AndroidFiles.generated(project), "source")
+        fun generated(project: Project) = KFiles.joinDir(project.directory, project.buildDirectory, "generated",
+                "source")
 
         if (project.buildConfig != null || context.variant.hasBuildConfig) {
             val buildConfigs = arrayListOf<BuildConfig>()
