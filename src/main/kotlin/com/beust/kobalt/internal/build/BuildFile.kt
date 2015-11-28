07fa5f7eb033f1e1292f70976a590795822e63ff
Cedric Beust <cedric@beust.com>
2015-11-28 10:58:34 -0800

    Any  build directory was being ignored, including sources... doh.

---
+++ src/main/kotlin/com/beust/kobalt/internal/build/BuildFile.kt
@@ -0,0 +1,17 @@
+package com.beust.kobalt.internal.build
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
+/**
+ * Sometimes, build files are moved to temporary files, so we give them a specific name for clarity.
+ */
+class BuildFile(val path: Path, val name: String) {
+    public fun exists() : Boolean = Files.exists(path)
+
+    public val lastModified : Long
+        get() = Files.readAttributes(path, BasicFileAttributes::class.java).lastModifiedTime().toMillis()
+
+    public val directory : File get() = path.toFile().directory
+}
