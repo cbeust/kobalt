fc7df24e1aaa18f6e8e0a8d10d9fcd05f4360179
Cedric Beust <cedric@beust.com>
2015-11-30 21:46:40 -0800

    Doc.

--- src/main/kotlin/com/beust/kobalt/api/IRepoContributor.kt
+++ src/main/kotlin/com/beust/kobalt/api/IRepoContributor.kt
@@ -6,6 +6,11 @@ import java.net.URI
  * Plugins that add their own repos.
  */
 interface IRepoContributor : IContributor {
+    /**
+     * Note that the project passed might be null because this contributor is called twice:
+     * before the build file gets parsed (so we don't have any projects yet) and after the
+     * build file has been parsed (then it gets called once for each project discovered).
+     */
     fun reposFor(project: Project?) : List<URI>
 }
 
