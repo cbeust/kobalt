758e35a7db5a2f1d9347ba7ab2fc80ecf17d1a2d
Cedric Beust <cedric@beust.com>
2015-12-06 00:05:13 -0800

    Comment.

--- src/main/kotlin/com/beust/kobalt/api/PluginProperties.kt
+++ src/main/kotlin/com/beust/kobalt/api/PluginProperties.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 /**
  * Plugins can publish to and read from this object in order to exchange information. Keys stored in
- * these maps should be annotated with @ExportedProperty.
+ * these maps should be annotated with @ExportedPluginProperty.
  */
 class PluginProperties @Inject constructor() {
     private val pluginProperties = hashMapOf<String, HashMap<String, Any>>()
