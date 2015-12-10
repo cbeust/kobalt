aafb06fada855b8bdab7707c845393328df27dba
Christoph Sturm <me@christophsturm.com>
2015-12-10 08:40:24 +0100

    get rid of the return by making the throw clause bigger.
    I'm not sure what version I like better. small try blocks are also nice.

--- src/main/kotlin/com/beust/kobalt/misc/CheckVersions.kt
+++ src/main/kotlin/com/beust/kobalt/misc/CheckVersions.kt
@@ -1,10 +1,10 @@
 package com.beust.kobalt.misc
 
-import com.beust.kobalt.api.IClasspathDependency
+import com.beust.kobalt.KobaltException
 import com.beust.kobalt.api.Project
 import com.beust.kobalt.maven.DepFactory
-import com.beust.kobalt.maven.dependency.MavenDependency
 import com.beust.kobalt.maven.MavenId
+import com.beust.kobalt.maven.dependency.MavenDependency
 import javax.inject.Inject
 
 /**
@@ -21,19 +21,17 @@ public class CheckVersions @Inject constructor(val depFactory : DepFactory,
             arrayListOf(it.compileDependencies, it.testDependencies).forEach { cds ->
                 cds.forEach { compileDependency ->
                     if (MavenId.isMavenId(compileDependency.id)) {
-                        val dep: IClasspathDependency
                         try {
-                            dep = depFactory.create(compileDependency.shortId, executor, false /* go remote */)
-                        } catch(e: Exception) {
-                            log(1, "cannot resolve ${compileDependency.shortId}. ignoring")
-                            return@forEach
-                        }
-                        if (dep is MavenDependency) {
-                            val other = compileDependency as MavenDependency
-                            if (dep.id != compileDependency.id
-                                    && Versions.toLongVersion(dep.version) > Versions.toLongVersion(other.version)) {
-                                newVersions.add(dep.id)
+                            val dep = depFactory.create(compileDependency.shortId, executor, false /* go remote */)
+                            if (dep is MavenDependency) {
+                                val other = compileDependency as MavenDependency
+                                if (dep.id != compileDependency.id
+                                        && Versions.toLongVersion(dep.version) > Versions.toLongVersion(other.version)) {
+                                    newVersions.add(dep.id)
+                                }
                             }
+                        } catch(e: KobaltException) {
+                            log(1, "cannot resolve ${compileDependency.shortId}. ignoring")
                         }
                     }
                 }
