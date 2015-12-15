b3bffad3414bd5c27a6301e64717b4b786f24672
Cedric Beust <cedric@beust.com>
2015-12-16 01:40:03 +0400

    Better SpekRunner.

--- src/main/kotlin/com/beust/kobalt/internal/JUnitRunner.kt
+++ src/main/kotlin/com/beust/kobalt/internal/JUnitRunner.kt
@@ -5,15 +5,16 @@ import com.beust.kobalt.api.IClasspathDependency
 import com.beust.kobalt.api.KobaltContext
 import com.beust.kobalt.api.Project
 
-public class JUnitRunner() : GenericTestRunner() {
+open public class JUnitRunner() : GenericTestRunner() {
 
     override val mainClass = "org.junit.runner.JUnitCore"
 
+    open val dependencyName = "junit"
+
     override fun affinity(project: Project, context: KobaltContext) =
-            if (project.testDependencies.any { it.id.contains("junit")}) IAffinity.DEFAULT_POSITIVE_AFFINITY
+            if (project.testDependencies.any { it.id.contains(dependencyName)}) IAffinity.DEFAULT_POSITIVE_AFFINITY
             else 0
 
     override fun args(project: Project, classpath: List<IClasspathDependency>) = findTestClasses(project, classpath)
-
 }
 
--- src/main/kotlin/com/beust/kobalt/internal/SpekRunner.kt
+++ src/main/kotlin/com/beust/kobalt/internal/SpekRunner.kt
@@ -1,19 +1,6 @@
 package com.beust.kobalt.internal
 
-import com.beust.kobalt.api.IAffinity
-import com.beust.kobalt.api.IClasspathDependency
-import com.beust.kobalt.api.KobaltContext
-import com.beust.kobalt.api.Project
-
-public class SpekRunner() : GenericTestRunner() {
-
-    override val mainClass = "org.junit.runner.JUnitCore"
-
-    override fun affinity(project: Project, context: KobaltContext) =
-            if (project.testDependencies.any { it.id.contains("org.jetbrains.spek")})IAffinity.DEFAULT_POSITIVE_AFFINITY
-            else 0
-
-    override fun args(project: Project, classpath: List<IClasspathDependency>) = findTestClasses(project, classpath)
-
+public class SpekRunner() : JUnitRunner() {
+    override val dependencyName = "org.jetbrains.spek"
 }
 
