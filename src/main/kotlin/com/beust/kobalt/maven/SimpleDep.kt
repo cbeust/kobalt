4fb5dbf7bc0d0b7120b5ed2e8661c6c072fb0f7c
Cedric Beust <cedric@beust.com>
2015-12-16 21:36:50 +0400

    NPE.

--- src/main/kotlin/com/beust/kobalt/maven/SimpleDep.kt
+++ src/main/kotlin/com/beust/kobalt/maven/SimpleDep.kt
@@ -11,20 +11,20 @@ open class SimpleDep(open val mavenId: MavenId) : UnversionedDep(mavenId.groupId
 
     val version: String get() = mavenId.version!!
 
-    private fun toFile(v: String, s: String, suffix: String) : String {
+    private fun toFile(v: String, snapshotVersion: String?, suffix: String) : String {
         val fv = if (v.contains("SNAPSHOT")) v.replace("SNAPSHOT", "") else v
         val result = Strings.join("/", arrayListOf(toDirectory(v, false) +
-                artifactId + "-" + fv + s + suffix))
+                artifactId + "-" + fv + (snapshotVersion ?: "") + suffix))
         return result
     }
 
     fun toPomFile(v: String) = toFile(v, "", ".pom")
 
-    fun toPomFile(r: RepoFinder.RepoResult) = toFile(r.version!!.version, r.snapshotVersion!!.version, ".pom")
+    fun toPomFile(r: RepoFinder.RepoResult) = toFile(r.version!!.version, r.snapshotVersion?.version, ".pom")
 
     fun toJarFile(v: String = version) = toFile(v, "", suffix)
 
-    fun toJarFile(r: RepoFinder.RepoResult) = toFile(r.version!!.version, r.snapshotVersion!!.version, suffix)
+    fun toJarFile(r: RepoFinder.RepoResult) = toFile(r.version!!.version, r.snapshotVersion?.version, suffix)
 
     fun toPomFileName() = "$artifactId-$version.pom"
 
