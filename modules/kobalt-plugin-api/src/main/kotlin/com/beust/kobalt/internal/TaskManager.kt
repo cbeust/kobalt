6343d4f0833987f0aa90b7e48815a64c293265fd
Cedric Beust <cedric@beust.com>
2015-12-21 08:54:59 +0400

    Fix log.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/internal/TaskManager.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/internal/TaskManager.kt
@@ -367,7 +367,7 @@ class TaskWorker(val tasks: List<PluginTask>, val dryRun: Boolean) : IWorker<Plu
     override fun call() : TaskResult2<PluginTask> {
         if (tasks.size > 0) {
             tasks[0].let {
-                log(2, AsciiArt.taskColor(AsciiArt.horizontalSingleLine + " ${it.project.name}:${it.name}"))
+                log(1, AsciiArt.taskColor(AsciiArt.horizontalSingleLine + " ${it.project.name}:${it.name}"))
             }
         }
         var success = true
