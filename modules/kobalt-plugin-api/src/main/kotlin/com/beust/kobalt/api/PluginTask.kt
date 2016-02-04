02995ce6cbd43b3ba54eb9737a5f9efe14bcd13b
Cedric Beust <cedric@beust.com>
2016-02-04 21:34:45 +0400

    Better toString().

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/PluginTask.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/PluginTask.kt
@@ -11,6 +11,6 @@ abstract public class PluginTask : Callable<TaskResult2<PluginTask>> {
     abstract val project: Project
 
     override public fun toString() : String {
-        return toString("PluginTask", "id", project.name + ":" + name)
+        return toString("PluginTask", "plugin", plugin.name, "id", project.name + ":" + name)
     }
 }
