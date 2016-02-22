bfb79b6dcddbfbfe19b947f96442a3f527fcfe72
Cedric Beust <cedric@beust.com>
2016-02-23 00:56:17 +0400

    Build fix.

--- src/main/kotlin/com/beust/kobalt/app/Templates.kt
+++ src/main/kotlin/com/beust/kobalt/app/Templates.kt
@@ -4,15 +4,14 @@ import com.beust.kobalt.api.ITemplate
 import com.beust.kobalt.api.ITemplateContributor
 import com.beust.kobalt.app.java.JavaBuildGenerator
 import com.beust.kobalt.app.kotlin.KotlinBuildGenerator
-import com.beust.kobalt.app.remote.DependencyData
 import com.beust.kobalt.internal.PluginInfo
 import com.beust.kobalt.misc.log
 import com.google.common.collect.ArrayListMultimap
 import com.google.inject.Inject
 
 class Templates @Inject constructor() : ITemplateContributor {
-    override val templates = listOf(JavaBuildGenerator(), KotlinBuildGenerator(),
-            //IdeaFilesTemplate()
+    override val templates = listOf(JavaBuildGenerator(), KotlinBuildGenerator()
+            //, IdeaFilesTemplate()
             )
 
     fun list(pluginInfo: PluginInfo) {
