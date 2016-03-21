a84bf05dd7e09d252138d2921f1a5e2ca43bec6b
Cedric Beust <cedric@beust.com>
2016-03-21 22:00:38 +0400

    Fix repos.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/Constants.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/Constants.kt
@@ -7,15 +7,15 @@ object Constants {
     val BUILD_FILE_DIRECTORY = "kobalt/src"
     val BUILD_FILE_PATH = KFiles.joinDir(BUILD_FILE_DIRECTORY, BUILD_FILE_NAME)
 
-    internal val DEFAULT_REPOS = listOf(
+    internal val DEFAULT_REPOS = listOf<String>(
             "http://repo1.maven.org/maven2/",
             "https://maven-central.storage.googleapis.com/",
             "https://jcenter.bintray.com/"
 
-            // The following two repos contain snapshots, never include them or default
-//            "https://repository.jboss.org/nexus/content/repositories/root_repository/",
-//            "http://repository.jetbrains.com/all/"
-//            "https://oss.sonatype.org/content/repositories/snapshots/"
+            // The following repos contain snapshots, don't include them by default
+//            , "https://repository.jboss.org/nexus/content/repositories/root_repository/"
+//            , "http://repository.jetbrains.com/all/"
+//            , "https://oss.sonatype.org/content/repositories/snapshots/"
     )
 
 }
