b3aab958528f8ff08933db54d2329ed7524a855a
Cedric Beust <cedric@beust.com>
2016-03-31 02:37:16 -0800

    Doc.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/IncrementalTaskInfo.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/IncrementalTaskInfo.kt
@@ -3,8 +3,8 @@ package com.beust.kobalt
 import com.beust.kobalt.api.Project
 
 /**
- * @param inputChecksum The checksum for the input to this task. It gets compared against the checksum of
- *  if they differ, the task gets run. If they are equal, outputChecksums are then compared.
+ * @param inputChecksum The checksum for the input to this task. It gets compared against the previous checksum
+ * calculated by Kobalt. If they differ, the task gets run. If they are equal, outputChecksums are then compared.
  * @param outputChecksum The checksum for the output of this task. If null, the output is absent
  * and the task will be run. If non null, it gets compared against the checksum of the previous run and
  * if they differ, the task gets run. Note that this parameter is a closure and not a direct value
