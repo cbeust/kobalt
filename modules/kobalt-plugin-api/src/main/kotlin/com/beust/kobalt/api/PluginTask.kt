63bd66cb02a08e8233e0e75bec6be24564f4f9f1
Cedric Beust <cedric@beust.com>
2016-04-12 18:25:31 -0700

    public

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/PluginTask.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/PluginTask.kt
@@ -4,13 +4,13 @@ import com.beust.kobalt.internal.TaskResult2
 import com.beust.kobalt.misc.toString
 import java.util.concurrent.Callable
 
-abstract public class PluginTask : Callable<TaskResult2<PluginTask>> {
+abstract class PluginTask : Callable<TaskResult2<PluginTask>> {
     abstract val plugin: IPlugin
     open val name: String = ""
     open val doc: String = ""
     abstract val project: Project
 
-    override public fun toString() : String {
+    override fun toString() : String {
         return toString("PluginTask", "plugin", plugin.name, "id", project.name + ":" + name)
     }
 }
