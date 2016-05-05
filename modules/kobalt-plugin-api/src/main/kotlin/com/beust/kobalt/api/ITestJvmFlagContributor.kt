dd65e3bbc77bc6f683d6b2371252184d76df1dc3
Cedric Beust <cedric@beust.com>
2016-05-04 23:25:33 -0800

    Comments.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/ITestJvmFlagContributor.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/ITestJvmFlagContributor.kt
@@ -4,5 +4,9 @@ package com.beust.kobalt.api
  * Plug-ins that add flags to the JVM used to run tests should implement this interface.
  */
 interface ITestJvmFlagContributor : IContributor {
+    /**
+     * The list of JVM flags that will be added to the JVM when the tests get run. @param[currentFlags] is only here
+     * for convenience, in case you need to look at the current JVM flags before adding your own flags.
+     */
     fun testJvmFlagsFor(project: Project, context: KobaltContext, currentFlags: List<String>) : List<String>
 }
