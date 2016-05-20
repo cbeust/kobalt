b36e417f0c7e4d638433bfcbe60ff469e63dc997
Cedric Beust <cedric@beust.com>
2016-05-19 22:22:00 -0800

    Simplify.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/Task.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/api/Task.kt
@@ -2,8 +2,6 @@ package com.beust.kobalt.api
 
 import com.beust.kobalt.misc.toString
 
-data public class Task(val pluginName: String, val taskName: String) {
-    override public fun toString() : String {
-        return toString("Task", pluginName, taskName)
-    }
+data class Task(val pluginName: String, val taskName: String) {
+    override fun toString() = toString("Task", pluginName, taskName)
 }
