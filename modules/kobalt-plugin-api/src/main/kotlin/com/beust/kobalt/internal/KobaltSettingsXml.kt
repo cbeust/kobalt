0d2a206c5edc657b9644927e2f1bdc3f3e50c91d
Cedric Beust <cedric@beust.com>
2016-06-29 22:12:17 -0800

    Update to Kotlin 1.0.3.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/internal/KobaltSettingsXml.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/internal/KobaltSettingsXml.kt
@@ -27,7 +27,7 @@ class KobaltSettingsXml {
     var proxies: ProxiesXml? = null
 
     @XmlElement(name = "kobalt-compiler-version") @JvmField
-    var kobaltCompilerVersion: String = "1.0.2-1"
+    var kobaltCompilerVersion: String = "1.0.3"
 
     @XmlElement(name = "kobalt-compiler-repo") @JvmField
     var kobaltCompilerRepo: String? = null
