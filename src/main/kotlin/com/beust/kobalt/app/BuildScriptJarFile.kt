3975f733181f764c031362d0b4c8bcb040218a2e
Cedric Beust <cedric@beust.com>
2016-07-12 03:48:30 -0800

    Comments.

--- src/main/kotlin/com/beust/kobalt/app/BuildScriptJarFile.kt
+++ src/main/kotlin/com/beust/kobalt/app/BuildScriptJarFile.kt
@@ -3,6 +3,13 @@ package com.beust.kobalt.app
 import java.io.File
 import java.io.FileWriter
 
+/**
+ * A wrapper around buildScript.jar. Additionally, this class takes care of the "profiles" file that
+ * gets saved alongside buildScript.jar and which keeps track of which profiles were active when this
+ * jar file gets generated. With this file, Kobalt can accurately decide when the jar file should be
+ * regenerated if the user is specifying different profiles than the ones that were used to compile that
+ * jar file.
+ */
 class BuildScriptJarFile(val jarFile: File) {
     val file = File(jarFile.parent, "profiles")
 
