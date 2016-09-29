4df3baf685f977a4b3d3d3b3daf5bcdec7dcf645
Cedric Beust <cedric@beust.com>
2016-09-29 10:53:02 -0700

    Travis build fix attempt

--- build-travis.sh
+++ build-travis.sh
@@ -1,3 +1,5 @@
+ulimit -s 1082768
+
 #java -Xmx2048m -jar $(dirname $0)/kobalt/wrapper/kobalt-wrapper.jar $* clean
 java -Xmx2048m -jar $(dirname $0)/kobalt/wrapper/kobalt-wrapper.jar $* clean assemble test --parallel
 
