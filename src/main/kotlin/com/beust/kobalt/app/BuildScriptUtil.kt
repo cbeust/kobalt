205f10a53d0e604a80697be8c9c4c8d609de00b8
Cedric Beust <cedric.beust@le.com>
2017-02-17 13:32:51 -0800

    Reformat.

--- src/main/kotlin/com/beust/kobalt/app/BuildScriptUtil.kt
+++ src/main/kotlin/com/beust/kobalt/app/BuildScriptUtil.kt
@@ -84,7 +84,8 @@ class BuildScriptUtil @Inject constructor(val plugins: Plugins, val files: KFile
             }.forEach { cls ->
                 cls.methods.forEach { method ->
                     // Invoke vals and see if they return a Project
-                    if (method.name.startsWith("get") && Modifier.isStatic(method.modifiers) && method.parameterCount == 0) {
+                    if (method.name.startsWith("get") && Modifier.isStatic(method.modifiers)
+                            && method.parameterCount == 0) {
                         try {
                             val r = method.invoke(null)
                             if (r is Project) {
