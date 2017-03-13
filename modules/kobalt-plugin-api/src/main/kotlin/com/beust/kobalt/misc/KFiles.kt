1fb4278cd62ae4c4779120d9361b713acc4d1585
Cedric Beust <cedric.beust@le.com>
2017-03-13 11:26:32 -0700

    Investigate the TeamCity build failure.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/misc/KFiles.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/misc/KFiles.kt
@@ -259,7 +259,8 @@ class KFiles {
          * The build location for build scripts is .kobalt/build
          */
         fun findBuildScriptLocation(buildFile: BuildFile, jarFile: String) : String {
-            val result = joinDir(buildFile.dotKobaltDir.absolutePath, KFiles.SCRIPT_BUILD_DIR, jarFile)
+            val result = joinDir(File(".").absolutePath,buildFile.dotKobaltDir.absolutePath,
+                    KFiles.SCRIPT_BUILD_DIR, jarFile)
             kobaltLog(2, "Script jar file: $result")
             return result
         }
