43b037171c65335090f11f7870b3d3a3729ff4a4
Cedric Beust <cedric@beust.com>
2017-03-18 23:18:01 -0700

    Reformat.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/internal/ActorUtils.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/internal/ActorUtils.kt
@@ -1,7 +1,6 @@
 package com.beust.kobalt.internal
 
 import com.beust.kobalt.api.IProjectAffinity
-import com.beust.kobalt.api.ISimpleAffinity
 import com.beust.kobalt.api.KobaltContext
 import com.beust.kobalt.api.Project
 
@@ -11,19 +10,13 @@ class ActorUtils {
          * Return the plug-in actor with the highest affinity.
          */
         fun <T : IProjectAffinity> selectAffinityActor(project: Project, context: KobaltContext, actors: List<T>)
-            = actors.maxBy { it.affinity(project, context) }
+                = actors.maxBy { it.affinity(project, context) }
 
         /**
          * Return all the plug-in actors with a non zero affinity sorted from the highest to the lowest.
          */
         fun <T : IProjectAffinity> selectAffinityActors(project: Project, context: KobaltContext, actors: List<T>)
                 = actors.filter { it.affinity(project, context) > 0 }
-                        .sortedByDescending { it.affinity(project, context) }
-
-        /**
-         * Return the plug-in actor with the highest affinity.
-         */
-        fun <T : ISimpleAffinity<A>, A> selectAffinityActor(actors: List<T>, arg: A) = actors.maxBy { it.affinity(arg) }
+                .sortedByDescending { it.affinity(project, context) }
     }
-
 }
