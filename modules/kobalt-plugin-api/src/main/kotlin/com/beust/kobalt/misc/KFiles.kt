981bbbacb65dce802b6f8dd52f72a921d4927884
Cedric Beust <cedric@beust.com>
2017-03-26 10:18:45 -0700

    Normalize.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/misc/KFiles.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/misc/KFiles.kt
@@ -115,8 +115,8 @@ class KFiles {
          */
         fun joinFileAndMakeDir(vararg ts: String) = joinDir(joinAndMakeDir(ts.slice(0..ts.size - 2)), ts[ts.size - 1])
 
-        fun fixSlashes(f: File) = KFiles.fixSlashes(f.path)
-        fun fixSlashes(s: String) = s.replace('\\', '/')
+        fun fixSlashes(f: File) = f.normalize().path.replace('\\', '/')
+        fun fixSlashes(s: String) = fixSlashes(File(s))
 
         fun makeDir(dir: String, s: String? = null) =
                 (if (s != null) File(dir, s) else File(dir)).apply { mkdirs() }
