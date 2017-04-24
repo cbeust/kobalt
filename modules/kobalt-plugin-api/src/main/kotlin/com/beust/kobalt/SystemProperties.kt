ec7d24a6f8d67e1ce43f29c59994ff686020c2b9
Cedric Beust <cedric.beust@le.com>
2017-04-24 09:48:23 -0700

    Make sure JAVA_HOME points to the JDK and not the JRE.
    Should fix https://github.com/cbeust/kobalt/issues/428.

--- modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/SystemProperties.kt
+++ modules/kobalt-plugin-api/src/main/kotlin/com/beust/kobalt/SystemProperties.kt
@@ -2,10 +2,16 @@ package com.beust.kobalt
 
 class SystemProperties {
     companion object {
-        val javaBase =
-                System.getenv("JAVA_HOME")
-                ?: System.getProperty("java.home")
-                ?: throw IllegalArgumentException("JAVA_HOME not defined")
+        val javaBase : String
+            get() {
+                val jh = System.getenv("JAVA_HOME")
+                    ?: System.getProperty("java.home")
+                    ?: throw IllegalArgumentException("JAVA_HOME not defined")
+                val result =
+                    if (jh.toLowerCase().endsWith("jre")) jh.substring(0, jh.length - 4)
+                    else jh
+                return result
+            }
         val javaVersion = System.getProperty("java.version")
         val homeDir = System.getProperty("user.home")
         val tmpDir = System.getProperty("java.io.tmpdir")
