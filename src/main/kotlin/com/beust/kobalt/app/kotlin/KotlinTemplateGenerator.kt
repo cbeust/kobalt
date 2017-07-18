9e6b3d26db91afcba416b02a0ef53df9b1585ea5
Tim Keating <mrtact@gmail.com>
2017-07-18 00:26:01 -0500

    Fixes #451 -- add stdlib and runtime deps to the Kotlin project template

--- src/main/kotlin/com/beust/kobalt/app/kotlin/KotlinTemplateGenerator.kt
+++ src/main/kotlin/com/beust/kobalt/app/kotlin/KotlinTemplateGenerator.kt
@@ -1,10 +1,15 @@
 package com.beust.kobalt.app.kotlin
 
 import com.beust.kobalt.app.LanguageTemplateGenerator
+import com.beust.kobalt.maven.Pom
 
 class KotlinTemplateGenerator : LanguageTemplateGenerator() {
     override val defaultSourceDirectories = hashSetOf("src/main/kotlin")
     override val defaultTestDirectories = hashSetOf("src/test/kotlin")
+    override val mainDependencies = arrayListOf(
+            Pom.Dependency("org.jetbrains.kotlin","kotlin-runtime", null, "1.1.3"),
+            Pom.Dependency("org.jetbrains.kotlin", "kotlin-stdlib", null, "1.1.3"))
+    override val testDependencies = arrayListOf<Pom.Dependency>()
     override val directive = "project"
     override val templateName = "kotlin"
     override val templateDescription = "Generate a simple Kotlin project"
