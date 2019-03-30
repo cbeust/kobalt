a5c0bd921ff29c5435936cf8978a89e7418a0e9c
Erik C. Thauvin <erik@thauvin.net>
2019-03-29 23:52:02 -0700

    Added Gradle project based on the POM.

---
+++ modules/wrapper/build/docs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.beust.kobalt.wrapper","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"com.beust.kobalt.wrapper","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ modules/wrapper/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.beust.kobalt.wrapper","l":"Main"}]
