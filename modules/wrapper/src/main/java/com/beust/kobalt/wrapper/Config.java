1649615c44c68b2d3b5d7bb31646be5e1b415cbe
Cedric Beust <cedric@beust.com>
2019-07-20 12:37:30 -0700

    public

--- modules/wrapper/src/main/java/com/beust/kobalt/wrapper/Config.java
+++ modules/wrapper/src/main/java/com/beust/kobalt/wrapper/Config.java
@@ -16,7 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-class Config {
+public class Config {
 
   static Proxy getProxy() {
     String configFilePath = getConfigFilePath();
